358fb959e7d8166877d5e3ca559307bd007f7392
Andy Lin <alin21@stuy.edu>
2020-11-19 13:29:37 -0500

    added gcd:
    :
    :q

--- RationalNumber.java
+++ RationalNumber.java
@@ -55,9 +55,19 @@ public class RationalNumber extends RealNumber
   *@return the value of the GCD
   */
   private static int gcd(int a, int b){
-    /*use euclids method or a better one*/
-    http://sites.math.rutgers.edu/~greenfie/gs2004/euclid.html
-    return 0;
+    int temp = 0;
+    if(a<b){
+      temp = a;
+      a = b;
+      b = temp
+    }
+    temp = a%b;
+    while (temp != 0){
+      a = b;
+      b = temp;
+      temp = a%b
+    }
+    return b;
   }
 
   /**
