11fb447fefbdec536a98ad9464d8e8f8a457b5ef
Andy Lin <alin21@stuy.edu>
2020-11-23 15:28:16 -0500

    xfered equals

--- Number.java
+++ Number.java
@@ -6,7 +6,13 @@ public abstract class Number{
   return a positive value when this Number is larger than the other Number
   */
   public int compareTo(Number other){
-    //TO BE IMPLEMENTED
+    if (this.getValue() == other.getValue()){
+      return 0;
+    }else if (this.getValue() < other.getValue()){
+      return this.getValue()*-1;
+    }else{
+      return this.getValue();
+    }
   }
 
   /*
@@ -15,5 +21,6 @@ public abstract class Number{
   *Special case: if one is exactly zero, the other must be exactly zero.
   */
   public boolean equals(Number other){
-    //TO BE IMPLEMENTED  }
+    return this.getValue() == other.getValue();
+  }
 }
